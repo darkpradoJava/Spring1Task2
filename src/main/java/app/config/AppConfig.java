14d0a27b993f23dbbf322dde1cce3b54db6255c1
darkpradoJava <darkpradoJava@yandex.ru>
2020-01-06 16:37:30 +0300

    Spring1Task2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -4,7 +4,6 @@ import app.model.Timer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
